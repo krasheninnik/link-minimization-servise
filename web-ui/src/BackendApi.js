b749801f3741b969a20fb66643d22eea375d0be8
Nikita Krasheninnik <krasheninnik99@gmail.com>
2021-05-28 12:41:37 +0700

    backendApi refactoring

--- web-ui/src/BackendApi.js
+++ web-ui/src/BackendApi.js
@@ -1,4 +1,3 @@
-const https = require("https");
 const queryString = require("query-string");
 
 class BackendApi {
@@ -8,60 +7,48 @@ class BackendApi {
       // res.status >= 200 && res.status < 300
       return res;
     } else {
-      console.log("what there happens?");
       throw Error(res.statusText);
     }
   }
 
-  async postData(url = "") {
+  async postRequest(url) {
+    // set request options
     const options = {
       method: "POST",
-      //agent: new https.Agent({
-      //  rejectUnauthorized: false,
-      //}),
     };
-    // Default options are marked with *
+
+    // request:
     let response;
     try {
       response = await fetch(url, options);
     } catch (e) {
-      console.log("hello from the core of request");
       throw e;
     }
-    console.log(response);
-    console.log("-------------------");
+
     return response;
   }
 
   async post(longUrl) {
+    // set post options:
     const method = "/api/urltransformation";
     const paramsObject = { longUrl: longUrl };
     const params = queryString.stringify(paramsObject);
     const endpoint = this.hostname + method + "?" + params;
 
-    console.log(endpoint);
-
+    // request
     let result;
     try {
-      result = await this.postData(endpoint);
-      console.log("after post data");
-      console.log(result);
-
+      result = await this.postRequest(endpoint);
       result = this.checkStatus(result);
-      console.log("after check status");
-
       result = await result.json();
-      console.log("after json()");
-      console.log(result);
-      console.log(result.shortUrl);
 
+      // return result of success api call
       return {
         success: true,
         message: result.shortUrl,
       };
     } catch (e) {
-      console.log("hellllo mazafucker" + JSON.stringify(e));
-      console.log(e.message);
+      // return error of failed api call
       return {
         success: false,
         message: e.message,
